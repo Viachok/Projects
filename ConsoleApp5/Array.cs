92ed10365a0ad1130a84a9528db5cc7c334c1ec4
Student <Student@KLASS-3-11>
2019-12-07 19:21:25 +0700

    Добавил новых методов и описания ко всем

--- ConsoleApp5/Array.cs
+++ ConsoleApp5/Array.cs
@@ -21,26 +21,26 @@ namespace ConsoleApp5
             for (int i = 0; i < index; i++)
                 _arr[i] = 0;
         }
-        public void print()
+        public void print()//Выводит все значения в массиве
         {
             foreach (int el in _arr)
                 Console.Write(el + " ");
         }
-        public int AllSum()
+        public int AllSum()//Складывает все значения в массиве
         {
             int sum = 0;
             foreach(int x in _arr)
                 sum += x;
             return sum;
         }
-        public int AllMul()
+        public long AllMul()// Умножает все значния в массиве
         {
             int sum = 0;
             foreach (int x in _arr)
                 sum *= x;
             return sum;
         }
-        public int Max()
+        public int Max()// Находит макисмальное значение в массиве
         {
             int max = 0;
             foreach (int x in _arr)
@@ -48,7 +48,7 @@ namespace ConsoleApp5
                     max = x;
             return max;
         }
-        public int Min()
+        public int Min()// Находит минимальное значение в массиве
         {
             int min = 0;
             foreach (int x in _arr)
@@ -56,7 +56,7 @@ namespace ConsoleApp5
                     min = x;
             return min;
         }
-        public int Average()
+        public int Average()//Находит среднее арифметическое
         {
             int ave = 0;
             foreach (int x in _arr)
@@ -64,15 +64,23 @@ namespace ConsoleApp5
             ave = ave / _arr.Length;
             return ave;
         }
-        public int FindInd(int index)
+        public int FindInd(int value)//Если есть значение value возаращет его индекс в массиве
         {
             for(int i = 0; i < _arr.Length; i++)
             {
-                if (_arr[i] == index)
+                if (_arr[i] == value)
                     return i;
             }
-                    return -1;
+            return -1;
         }
-        public bool find
+        public bool FindVal(int value)//Если есть значение value возвращает true или false
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (_arr[i] == value)
+                    return true;
+            }
+            return false;
+        }
     }
 }
